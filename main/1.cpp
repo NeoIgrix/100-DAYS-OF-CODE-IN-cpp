292c3e52e0ed0e77832fd71692a191838a5bfe1a
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-21 09:58:45 +0530

    .

--- main/1.cpp
+++ main/1.cpp
@@ -1,8 +1,29 @@
+// Write a program to input two numbers and display their sum.
+
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
+float sum(float a, float b){
+    return a+b;
+}
 int main() {
-    
+    float a,b;
+    cin >> a >> b;
+    cout << sum(a,b);
     return 0;
 }
+/*#include <iostream>
+using namespace std;
+
+void sum(float a, float b) {
+    cout << a + b;
+}
+
+int main() {
+    float a, b;
+    cin >> a >> b;
+    sum(a, b);
+    return 0;
+}*/
+
