5787acea23afde9156b7d504b7636545896124ac
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-21 22:59:53 +0530

    .

---
+++ main/14.cpp
@@ -0,0 +1,10 @@
+// Write a program to input a character and check whether it is a vowel or consonant using if–else.
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
