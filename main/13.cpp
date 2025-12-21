29f47293c4a4f795c1b6048bd20bcef8e9122866
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-21 22:41:56 +0530

    .

---
+++ main/13.cpp
@@ -0,0 +1,10 @@
+// Write a program to input a year and check whether it is a leap year or not using conditional statements.
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
