3043f3f0f4114f96844b5fe40e6e91daf39ae87c
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-22 00:54:42 +0530

    .

--- main/18.cpp
+++ main/18.cpp
@@ -8,8 +8,25 @@ below 60: Grade F.*/
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
+void grade(int m){
+    if (m >=90 && m <=100)
+    {
+      cout << "GRADE A" << endl;  
+    }else if (m >=80 && m <=89)
+    {
+     cout << "GRADE B" << endl;
+    }else if (m >=70 && m <=79)
+    {
+     cout << "GRADE c" << endl;
+    }else if (m >=60 && m <=69)
+    {
+     cout << "GRADE d" << endl;
+    }else
+    cout << "GRADE f" << endl;
+}
 int main() {
-    
+    int m;
+    cin >> m;
+    grade(m);
     return 0;
 }
---
+++ main/19.cpp
@@ -0,0 +1,9 @@
+// Write a program to classify a triangle as Equilateral, Isosceles, or Scalene based on its side lengths.
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
