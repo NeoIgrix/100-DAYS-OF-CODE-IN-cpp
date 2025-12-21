4b2cf7ad848f9a442b7818a2f582ffcd5ce9f52b
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-21 22:51:17 +0530

    .

--- main/13.cpp
+++ main/13.cpp
@@ -3,8 +3,17 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+void leap(int n) {
+    if (n % 400 == 0 || (n % 4 == 0 && n % 100 != 0)) {
+        cout << "this is a leap year";
+    } else {
+        cout << "this is not a leap year";
+    }
+}
 
 int main() {
-    
+    int n;
+    cin >> n;
+    leap(n);
     return 0;
 }
