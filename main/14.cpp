fc40531e3894cd63de3d525d73b06a76183e4b44
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-21 23:07:01 +0530

    .

--- main/14.cpp
+++ main/14.cpp
@@ -3,8 +3,20 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
+void check(char n){
+    if (!isalpha(n)) {                 
+        cout << "Not an alphabet" << endl;
+        return;
+    }
+    if (n == 'a'||n == 'e'||n == 'i'||n == 'o'||n == 'u'||n == 'A'||n == 'E'||n == 'I'||n == 'O'||n == 'U')
+    {
+    cout << "GIVEN CHARACTER IS A VOWEL" << endl;
+    }else
+    cout << "GIVEN CHARACTER IS A consonant" << endl;
+}
 int main() {
-    
+    char n;
+    cin >> n;
+    check(n);
     return 0;
 }
