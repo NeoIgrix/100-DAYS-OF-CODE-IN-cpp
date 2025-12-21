71a4da483686faf5ad7cc0eec0ce1aed01ca2bac
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-22 00:48:00 +0530

    .

---
+++ main/18.cpp
@@ -0,0 +1,15 @@
+/*Write a program that accepts a percentage (0-100) and assigns a grade based on the following criteria: 
+90-100: Grade A 
+80-89: Grade B 
+70-79: Grade C 
+60-69: Grade D 
+below 60: Grade F.*/
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
