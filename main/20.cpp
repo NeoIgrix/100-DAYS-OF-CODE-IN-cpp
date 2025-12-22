923894f7cfd89973abb02c845321c31f13dc797d
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-22 22:57:02 +0530

    .

--- main/20.cpp
+++ main/20.cpp
@@ -3,8 +3,39 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+void day(int n){
+     switch (n)
+     {
+     case 1:
+        cout << "Monday" << endl;
+        break;
+     case 2:
+        cout << "tuesday" << endl;
+        break;
+     case 3:
+        cout << "wednesday" << endl;
+        break;
+     case 4:
+        cout << "thursday" << endl;
+        break;         
+     case 5:
+        cout << "friday" << endl;
+        break;
+     case 6:
+        cout << "saturday" << endl;
+        break;
+     case 7:
+        cout << "sunday" << endl;
+        break;   
 
+     default:
+     cout << "invalid" << endl;
+        break;
+     }
+}
 int main() {
-    
+    int n;
+    cin >> n;
+    day(n);
     return 0;
 }
