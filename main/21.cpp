2d57b0a4d896dfc943d5bdbd6ba365477eb00d86
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-22 23:04:26 +0530

    .

--- main/21.cpp
+++ main/21.cpp
@@ -26,3 +26,44 @@ int main() {
     month(n);
     return 0;
 }
+/*#include <iostream>
+using namespace std;
+
+bool isLeap(int year) {
+    return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+}
+
+void month(int m, int y) {
+    switch (m) {
+        case 1:  cout << "January - 31 days"; break;
+
+        case 2:
+            if (isLeap(y))
+                cout << "February - 29 days (Leap Year)";
+            else
+                cout << "February - 28 days";
+            break;
+
+        case 3:  cout << "March - 31 days"; break;
+        case 4:  cout << "April - 30 days"; break;
+        case 5:  cout << "May - 31 days"; break;
+        case 6:  cout << "June - 30 days"; break;
+        case 7:  cout << "July - 31 days"; break;
+        case 8:  cout << "August - 31 days"; break;
+        case 9:  cout << "September - 30 days"; break;
+        case 10: cout << "October - 31 days"; break;
+        case 11: cout << "November - 30 days"; break;
+        case 12: cout << "December - 31 days"; break;
+
+        default:
+            cout << "Invalid month number";
+    }
+}
+
+int main() {
+    int monthNo, year;
+    cin >> monthNo >> year;
+    month(monthNo, year);
+    return 0;
+}
+*/
