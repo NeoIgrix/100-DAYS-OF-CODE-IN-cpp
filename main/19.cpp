54a0bb48b10aa0dee49b12d1449506177ef76026
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-22 22:45:47 +0530

    .

--- main/19.cpp
+++ main/19.cpp
@@ -2,8 +2,26 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
+void classify(int side1 ,int side2, int side3){
+     if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) 
+     {
+        cout << "Not a valid triangle";
+     }
+     
+     else if (side1 == side2 && side1 == side3)
+     {
+        cout << "equilateral" << endl;
+     }else if (side1 == side2 || side1 == side3 || side2 == side3)
+     {
+        cout << "isoceles" << endl;
+     }else
+     cout << "scalene" << endl;
+     
+     
+}
 int main() {
-    
+    int side1,side2,side3;
+    cin >> side1 >> side2 >> side3;
+    classify(side1,side2,side3);
     return 0;
 }
---
+++ main/20.cpp
@@ -0,0 +1,10 @@
+// Write a program to display the day of the week based on a number (1–7) using switch-case
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
