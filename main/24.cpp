83f4dafa78ad23a2cc4e923363e72a851a95e1a7
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-23 15:17:40 +0530

    .

--- main/24.cpp
+++ main/24.cpp
@@ -7,8 +7,24 @@ Above at ₹12/unit*/
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
+void bill(int u){
+     if (u <= 100)
+     {
+        cout << "bill is: " << 250.0 + (u*5) << endl;// 250 is base charge
+     }else if (u <= 200)
+     {
+        cout << "bill is: " << 500.0 + 250.0 + (u-100)*7 << endl;
+     }else if (u <= 300)
+     {
+        cout << "bill is: " <<500.0 + 700.0 + 250.0 + (u-200)*10 << endl;
+     }else
+     cout << "bill is: " << 1000 + 1200 + 250.0 + (u-300)*12 << endl;
+     
+}
 int main() {
-    
+    int u;
+    cout << "enter units consumed: " << endl;
+    cin >> u;
+    bill(u);
     return 0;
 }
