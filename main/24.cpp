482af81a8c0273705a6010a91ef05f23632962be
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-23 14:54:16 +0530

    .

---
+++ main/24.cpp
@@ -0,0 +1,14 @@
+/*Write a program to calculate electricity bill based on units consumed with these rates: 
+First 100 units at ₹5/unit 
+Next 100 units at ₹7/unit 
+Next 100 units at ₹10/unit 
+Above at ₹12/unit*/
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
