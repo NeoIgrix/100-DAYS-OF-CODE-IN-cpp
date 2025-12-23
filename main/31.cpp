47b9dd5fd8d8fe6845b74368c858715cecb07fd9
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-24 01:04:03 +0530

    .

---
+++ main/31.cpp
@@ -0,0 +1,31 @@
+// Write a program to take a number as input and print its equivalent binary representation.
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void reverse(int n){
+    int r = 0;
+    while(n>0)
+    {
+    int ld = n % 10 ;
+    r= (r*10) + ld ;
+    n/=10;
+    }
+    cout << r ;
+}
+void binary(int n){
+     int binrev = 0; 
+    while (n>0)
+     {
+        int rem = n%2;
+        binrev = binrev*10 + rem ;
+        n/=2;
+     }
+     reverse(binrev);
+}
+int main() {
+    int n;
+    cin >> n;
+    binary(n);
+    return 0;
+}
