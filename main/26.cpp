eaa73daf62e3353eb7cc67e33a23af18f3903f92
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-23 19:38:21 +0530

    .

---
+++ main/26.cpp
@@ -0,0 +1,18 @@
+// Write a program to print numbers from 1 to n.
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void print(int n){
+    for (int i = 1; i <=n; i++)
+    {
+      cout << i << endl;
+    }
+    
+}
+int main() {
+    int n;
+    cin >> n;
+    print(n);
+    return 0;
+}
