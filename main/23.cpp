8888ccc5af1ecef4a9e3f96614f6741d0566141d
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-23 14:53:44 +0530

    .

--- main/23.cpp
+++ main/23.cpp
@@ -7,8 +7,29 @@ More than 30 days: Membership Cancelled.*/
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
+void fine(int n){
+    if (n <= 7)
+    {
+      cout << "No fine!\n";  
+    }else if (n > 7 && n <= 12)
+    {
+        cout << "Fine is ₹2/day i.e " << (n-7)*2 ;
+    }else if (n > 12 && n <= 17)
+    {
+        cout << "Fine is ₹4/day i.e " << (n-12)*4 ;
+    }else if (n > 17 && n <= 37)
+    {
+        cout << "Fine is ₹6/day i.e " << (n-17)*6 ;
+    }else if (n > 37)
+    {
+        cout << "membership is cancelled. Pay ₹150 to renew it  "  ;
+    }
     
+    
+}
+int main() {
+    int n;
+    cin >> n;
+    fine(n);
     return 0;
 }
