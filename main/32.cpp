da8792b10e39ab6b94272d8e257433e1c7fd0174
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-24 01:09:59 +0530

    .

---
+++ main/32.cpp
@@ -0,0 +1,28 @@
+// Write a program to check if a number is a palindrome.
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void palindrome(int n){
+    int original = n;
+    int r = 0;
+    while(n>0)
+    {
+    int ld = n % 10 ;
+    r= (r*10) + ld ;
+    n/=10;
+    }
+    cout << r ;
+    
+    if (original == r)
+    {
+        cout << "  palindrome" << endl;
+    }else
+    cout << "  invalid";
+}
+int main() {
+    int n;
+    cin >>n;
+    palindrome(n);
+    return 0;
+}
