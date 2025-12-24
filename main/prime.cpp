b5e9c5f8cdca7c610b2cd05235de52b945a6e41b
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-25 00:28:02 +0530

    .

--- main/prime.cpp
+++ main/prime.cpp
@@ -3,8 +3,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 void isprime(int n){
-    
+    if (n <= 1) {
+        cout << "composite\n";
+        return;
+    }
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            cout << "composite\n";
+            return;  // stop here
+        }
+    }
+
+    cout << "prime\n";  // only reaches this if no divisor is found
 }
+
 int main() {
     int n;
     cin >> n;
