caa262ea0a054556ec8fe98a9409afb2611dd451
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-24 21:48:54 +0530

    .

--- main/armstrong.cpp
+++ main/armstrong.cpp
@@ -2,8 +2,34 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
+int count(int n){
+    int c =0;
+    while (n!=0)
+    {
+     n/=10;
+     c++;
+    }
+    return c ;
+}
+void armstrong(int n){
+    int original = n ;
+     int sum = 0;
+     int digits = count(n);
+     while (n!=0)
+     {
+        int digit = n%10;// for extracting each digit
+        sum+= pow(digit,digits);
+        n/=10;
+     }
+     if (sum == original)
+     {
+        cout << "armstrong\n";
+     }else
+     cout << "invalid";
+}
 int main() {
-    
+    int n;
+    cin >> n ;
+    armstrong(n);
     return 0;
 }
