86a2683e2c16fe06d2a307bdd5b32468f4ef2e6f
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-24 22:09:23 +0530

    .

---
+++ main/prime.cpp
@@ -0,0 +1,13 @@
+// Write a program to check if a number is prime.
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void isprime(int n){
+    
+}
+int main() {
+    int n;
+    cin >> n;
+    isprime(n);
+    return 0;
+}
