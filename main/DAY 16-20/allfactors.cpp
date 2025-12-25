6e88416d5055c6bfc973cc01acaa3c1e9129b7ba
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-12-25 11:25:21 +0530

    .

---
+++ main/DAY 16-20/allfactors.cpp
@@ -0,0 +1,40 @@
+// Write a program to print all factors of a given number.
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void factor(int n){
+    for (int i = 1; i <= n; i++)
+    {
+      if (n%i == 0)
+      {
+        cout << i <<" " ;
+      } 
+    }
+    
+}
+int main() {
+    int n;
+    cin >> n;
+    factor(n);
+    return 0;
+}
+/*#include <iostream> // faster version
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            cout << i << " ";          // first factor
+
+            if (i != n / i)            // avoid duplicate for perfect squares
+                cout << n / i << " ";  // paired factor
+        }
+    }
+
+    return 0;
+}
+*/
